0a5d2e5f91c0d93be230f6cc0d1f5b83690bc4f3
Nicolas Pucino <44853536+npucino@users.noreply.github.com>
2021-02-24 19:52:30 +1100

    Update test_sandpyper.py

--- tests/test_sandpyper.py
+++ tests/test_sandpyper.py
@@ -8,14 +8,14 @@ import unittest
 import sandpyper
 
 
-class TestSandpyper(unittest.TestCase):
-    """Tests for `sandpyper` package."""
+#class TestSandpyper(unittest.TestCase):
+#    """Tests for `sandpyper` package."""
 
-    def setUp(self):
-        """Set up test fixtures, if any."""
+#    def setUp(self):
+#        """Set up test fixtures, if any."""
 
-    def tearDown(self):
-        """Tear down test fixtures, if any."""
+#    def tearDown(self):
+#        """Tear down test fixtures, if any."""
 
-    def test_000_something(self):
-        """Test something."""
+#    def test_000_something(self):
+#        """Test something."""
