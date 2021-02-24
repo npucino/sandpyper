cbb44f10f7dac4607f0ac40ed4bad422e9402562
Nicolas Pucino <44853536+npucino@users.noreply.github.com>
2021-02-24 20:22:37 +1100

    Update __init__.py

--- sandpyper/__init__.py
+++ sandpyper/__init__.py
@@ -1,11 +1,11 @@
 
 __version__= "0.0.4"
-__author__ = "Nicolas Pucino"
+__author__ = """Nicolas Pucino"""
 __email__ = 'npucino@deakin.edu.au'
 
-from . import dynamics
-from . import hotspot
-from . import labels
+#from . import dynamics
+#from . import hotspot
+#from . import labels
 # from . import outils
-from . import profile
-from . import volumetrics
+#from . import profile
+#from . import volumetrics
