abc95f607a5fce20b08846efdd495232fb8460ec
Nicolas Pucino <44853536+npucino@users.noreply.github.com>
2021-05-24 21:11:41 +1000

    Bump version: 0.0.8 → 0.0.9

--- sandpyper/__init__.py
+++ sandpyper/__init__.py
@@ -1,6 +1,6 @@
 
 """Profile module for sandpyper."""
 
-__version__ = " __version__ = 0.0.8"
+__version__ = " __version__ = 0.0.9"
 __author__ = """Nicolas Pucino"""
 __email__ = 'npucino@deakin.edu.au'
