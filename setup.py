8b4cea843a19271b436f6a186da5db4d104f3320
Nicolas Pucino <44853536+npucino@users.noreply.github.com>
2021-05-24 21:04:56 +1000

    Bump version: 0.0.7 → 0.0.8

--- setup.py
+++ setup.py
@@ -55,6 +55,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/npucino/sandpyper',
-    version='0.0.7',
+    version='0.0.8',
     zip_safe=False,
 )
