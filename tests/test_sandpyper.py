fd0b34183b60417fdfb1e0283e2190d9e6187759
Nicolas Pucino <44853536+npucino@users.noreply.github.com>
2021-08-20 12:00:00 +1000

    print folders paths

--- tests/test_sandpyper.py
+++ tests/test_sandpyper.py
@@ -217,18 +217,13 @@ class TestProfileSet(unittest.TestCase):
     def setUpClass(cls):
         ############################# Profile extraction ######################
         shoreline_leo_path = os.path.abspath("examples/test_data/shorelines/leo_shoreline_short.gpkg")
-        shoreline_mar_path = os.path.abspath('examples/test_data/shorelines/mar_shoreline_short.gpkg')
-        dsms_dir_path = os.path.abspath('examples/test_data/dsm_1m/')
-        orthos_dir_path = os.path.abspath('examples/test_data/orthos_1m')
-        transects_path = os.path.abspath('examples/test_data/transects/')
-        lod_mode=os.path.abspath('examples/test_data/lod_transects/')
-        label_corrections_path=os.path.abspath("examples/test_data/clean/label_corrections.gpkg")
-        watermasks_path=os.path.abspath("examples/test_data/clean/watermasks.gpkg")
-        shoremasks_path=os.path.abspath("examples/test_data/clean/shoremasks.gpkg")
-        test_pickled=os.path.abspath("examples/test_data/test.p")
-        P_test=pickle.load(open(test_pickled, "rb"))
+        shoreline_mar_path = os.path.abspath("examples/test_data/shorelines/mar_shoreline_short.gpkg")
+        dsms_dir_path = os.path.abspath("examples/test_data/dsm_1m/")
+        orthos_dir_path = os.path.abspath("examples/test_data/orthos_1m")
+        transects_path = os.path.abspath("examples/test_data/transects/")
+        lod_mode=os.path.abspath("examples/test_data/lod_transects/")
 
-
+        print(f"THIS IS THE CURRENT DIRECTORY FOR DSMS: {dsms_dir_path} AND TRANSECTS {transects_path} ")
 
         cls.P = ProfileSet(dirNameDSM=dsms_dir_path,
                         dirNameOrtho=orthos_dir_path,
@@ -326,17 +321,15 @@ class TestProfileDynamics(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
 
-        shoreline_leo_path = os.path.abspath("examples/test_data/shorelines/leo_shoreline_short.gpkg")
-        shoreline_mar_path = os.path.abspath('examples/test_data/shorelines/mar_shoreline_short.gpkg')
-        dsms_dir_path = os.path.abspath('examples/test_data/dsm_1m/')
-        orthos_dir_path = os.path.abspath('examples/test_data/orthos_1m')
-        transects_path = os.path.abspath('examples/test_data/transects/')
-        lod_mode=os.path.abspath('examples/test_data/lod_transects/')
+        transects_path = os.path.abspath("examples/test_data/transects/")
+        lod_mode=os.path.abspath("examples/test_data/lod_transects/")
         label_corrections_path=os.path.abspath("examples/test_data/clean/label_corrections.gpkg")
         watermasks_path=os.path.abspath("examples/test_data/clean/watermasks.gpkg")
         shoremasks_path=os.path.abspath("examples/test_data/clean/shoremasks.gpkg")
         test_pickled=os.path.abspath("examples/test_data/test.p")
         P_test=pickle.load(open(test_pickled, "rb"))
+
+        print(f"THIS IS THE CURRENT DIRECTORY TRANSECTS {transects_path} ")
 
         cls.D2 = ProfileDynamics(P_test, bins=5, method="JenksCaspall", labels=labels)
         cls.D2.compute_multitemporal(loc_full=loc_full, filter_class='sand')
