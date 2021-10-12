749041d88165e2d157931d6b385e09a11239cc41
npucino <nicolas.pucino@hotmail.com>
2021-10-13 09:21:32 +1100

    patch test for author in zenodo sandbox

--- setup.py
+++ setup.py
@@ -51,6 +51,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/npucino/sandpyper',
-    version='1.3.1',
+    version='1.3.2',
     zip_safe=False,
 )
